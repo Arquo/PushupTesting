c06e5c61213b3a2814ab98a185726155c886f63e
r <chenray2003@gmail.com>
2024-02-28 01:50:04 -0500

    database

--- database_functions.py
+++ database_functions.py
@@ -1,8 +1,8 @@
 import sqlite3
 import pandas as pd
 
-# Connect to SQLite database
 
+# add new user to the userinfo table, create user_data table
 def create_new_user(user_name, email, password):
     conn = sqlite3.connect("master_database.db")
     cursor = conn.cursor()
@@ -10,25 +10,29 @@ def create_new_user(user_name, email, password):
     cursor.execute('''CREATE TABLE IF NOT EXISTS userinfo_table (
                             id INTEGER PRIMARY KEY,
                             user_name TEXT,
-                            email TEXT,  
-                            password TEXT
+                            email TEXT,
+                            password
                          )''')
 
     cursor.execute('''INSERT INTO userinfo_table (user_name, email, password) VALUES (?, ?, ?)''',
                    (user_name, email, password))
+    conn.commit()
 
-    # initialize user data table, for now assume email is a primary key
-    cursor.execute('''CREATE TABLE IF NOT EXISTS {email} (
+    user_table_name = email.replace("@", "").replace(".com", "")
+    cursor.execute(f'''CREATE TABLE IF NOT EXISTS {user_table_name}(
                             date_time DATETIME PRIMARY KEY,
                             list_progress TEXT
-                         )'''.format(email=email))
+                         )''')
     conn.commit()
     cursor.close()
     conn.close()
 
 
 def add_training_data(email, date_time, list_progress):
-    conn = sqlite3.connect("master_database")
+    conn = sqlite3.connect("master_database.db")
     cursor = conn.cursor()
     cursor.execute("INSERT INTO {email}  (date_time, list_progress) VALUES (?, ?,?)".format(email=email)
                    , (date_time, list_progress))
+    conn.commit()
+    cursor.close()
+    conn.close()
--- operations.py
+++ operations.py
@@ -3,10 +3,7 @@ import pandas as pd
 import database_functions
 
 # Connect to SQLite database
-conn = sqlite3.connect('master_database.db')
-cursor = conn.cursor()
 
-
-database_functions.create_new_user("ray", "chihdia@gmail.com","dnqjnleqndl2")
+database_functions.create_new_user("ray", "ray@gmail.com","12231311")
 
 
